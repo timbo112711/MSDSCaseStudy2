3e378e117f6d3706c8212a9ca1c5303b0be9de1e
timbo112711 <tmcwilliams@smu.edu>
2017-04-11 23:16:40 -0400

    Add files via upload

---
+++ Rmatrix.R
@@ -0,0 +1,6 @@
+# The X matrix in R
+# Create a variable XX and store the cat of the matrix desired
+#   and then specify the number of rows and columns desired.
+#   Call the matrix XX
+XX = matrix(c(4,1,2,5,0,1,1,3,8,2,5,2), nrow = 3, ncol = 4)
+XX
