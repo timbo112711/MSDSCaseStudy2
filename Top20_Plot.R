33713e2d6da90cbf4096a731222cf265efe95bc7
timbo112711 <tmcwilliams@smu.edu>
2017-04-15 19:52:34 -0400

    Update Top20_Plot.R

--- Top20_Plot.R
+++ Top20_Plot.R
@@ -1,8 +1,8 @@
-# Attach the df 
-attach(Top20)
-
 # Select the first 20 rows 
 Top20 <- MonAvTempFinal[1:20,]
+
+# Attach the df 
+attach(Top20)
 
 # Drop the colunms with the Max and Min, it's easier to plot this way
 Max <- NULL
