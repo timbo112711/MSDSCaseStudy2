ae5af3845ba9fca5b6a5e3303eb5c10948f86cee
Tim McWilliams <tmcwilliams@smu.edu>
2017-04-23 13:01:08 -0400

    Update ALT_Plot.R

--- ALT_Plot.R
+++ ALT_Plot.R
@@ -1,4 +1,4 @@
 #Print bar graph by year
-ALt <- ggplot(data = Years_Temp, aes(x=Year, y= FTemp)) +geom_bar(stat="identity", alpha=0.25) +guides(fill = FALSE) +xlab("Years") + ylab("Average US Temps F") +geom_line(colour="red", size=1.5)
+ALt <- ggplot(data = US1990, aes(x=Year, y= FTemp)) +geom_bar(stat="identity", alpha=0.25) +guides(fill = FALSE) +xlab("Years") + ylab("Average US Temps F") +geom_line(colour="red", size=1.5)
 print(ALt)
 ggsave("altplot.png")
